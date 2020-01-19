eb43006f8958dac577998464af044348bf54e9ef
Unknown <electrikjesus@gmail.com>
2020-01-18 22:24:21 -0500

    Add OpenRazer drivers

---
+++ openrazer/daemon/setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+from setuptools import setup, find_packages
+
+setup(
+    name="openrazer_daemon",
+    version="2.7.0",
+    packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"])
+)
---
+++ openrazer/scripts/ci/install-check-deps.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -ex
+
+apt-get -y install \
+    astyle \
+    python3-pip
+
+pip3 install autopep8
+pip3 install pylint
+
+# pylint errors with 1.8+ on pylib/tests/integration_tests/test_device_manager.py about the missing 'coverage' module
+pip3 install coverage
