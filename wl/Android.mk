628a3650a66f6ba7259cd2c20ad97e9d7f80be98
Chih-Wei Huang <cwhuang@linux.org.tw>
2020-04-16 23:37:37 +0800

    Add more patches for wl driver
    From https://github.com/rpmfusion/wl-kmod

--- wl/Android.mk
+++ wl/Android.mk
@@ -19,22 +19,13 @@ WL_PATH := $(TARGET_OUT_INTERMEDIATES)/kmodule/wl
 WL_SRC := $(WL_PATH)/hybrid-v35$(if $(filter x86,$(TARGET_KERNEL_ARCH)),,_64)-nodebug-pcoem-6_30_223_271.tar.gz
 WL_LIB := $(WL_PATH)/lib$(if $(filter x86,$(TARGET_KERNEL_ARCH)),32,64)
 
-WL_PATCHES := \
-	wl.patch \
-	002-rdtscl.patch \
-	003-linux47.patch \
-	004-linux48.patch \
-	005-debian-fix-kernel-warnings.patch \
-	006-linux411.patch \
-	007-linux412.patch \
-	008-linux415.patch \
-	009-fix_mac_profile_discrepancy.patch \
+WL_PATCHES := $(wildcard $(LOCAL_PATH)/*.patch)
 
 $(WL_SRC):
 	@echo Downloading $(@F)...
-	$(hide) mkdir -p $(@D) && curl -k https://docs.broadcom.com/docs-and-downloads/docs/linux_sta/$(@F) > $@
+	$(hide) mkdir -p $(@D) && /usr/bin/curl -k https://docs.broadcom.com/docs-and-downloads/docs/linux_sta/$(@F) > $@ || rm -f $@
 
-$(WL_LIB): $(WL_SRC) $(addprefix $(LOCAL_PATH)/,$(WL_PATCHES))
+$(WL_LIB): $(WL_SRC) $(WL_PATCHES)
 	$(hide) tar zxf $< -C $(@D) --overwrite -m && \
 		rm -rf $@ && mv $(@D)/lib $@ && touch $@ && \
 		cat $(filter %.patch,$^) | patch -p1 -d $(@D)
